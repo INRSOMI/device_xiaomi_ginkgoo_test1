b559b98b78516434e628f90dc280cc160023a318
Aweasomi <aweasomegamespvp@gmail.com>
2023-12-24 11:41:20 -0500

    first commit

---
+++ AndroidProducts.mk
@@ -0,0 +1,12 @@
+#
+# Copyright (C) Evolution-X
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_ginkgo.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_ginkgo-user \
+    aosp_ginkgo-userdebug
